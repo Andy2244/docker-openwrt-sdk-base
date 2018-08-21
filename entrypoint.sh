1fdffef6a93f9167e76ea46c7792bfd3f610018e
Andy Walsh <andy.walsh44+github@gmail.com>
2018-08-21 21:06:12 +0200

    cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,28 +1,5 @@
 #!/bin/bash
 
-endchar="\$"
-if [ "$UID" = "0" ]; then
-	endchar="#"
-fi
-
-# Blue
-BG="\[\e[38;5;39m\]"
-# RGBA index:
-#BG="\[\e[38;2;255;142;58m\]"
-
-# Orange
-FG="\[\e[38;5;208m\]"
-# RGBA index:
-#FG="\[\e[38;2;0;174;255m\]"
-
-#PS1='🐳  \[\033[1;36m\]\h \[\033[1;34m\]\W\[\033[0;35m\] \[\033[1;36m\]# \[\033[0m\]'
-
-export PS1="🐳  $BG\u\[\e[0m\]@$FG\H ${BG}\w ${BG}$endchar \[\e[0;0m\]"
-if [ "${TERM:0:5}" = "xterm" ]; then
-	export PS1="\[\e]2;\u@\H :: \w\a\]$PS1"
-fi
-#####################################################
-
 # If GOSU_USER environment variable set to something other than 0:0 (root:root),
 # become user:group set within and exec command passed in args
 if [ -n "$GOSU_USER" -a "$GOSU_USER" != "0:0" ]; then
